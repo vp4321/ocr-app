924cd7f1d7f49e21bb37808499dd35678607158a
YASHMAROO <marooyash@yahoo.in>
2021-05-17 00:18:16 +0530

    [+]Added firebase oauth changed file structure

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,13 +2,31 @@ import React, { useState } from 'react'
 import Tesseract from 'tesseract.js'
 import ImageUploader from 'react-images-upload'
 import ClipLoader from 'react-spinners/ClipLoader'
-import './App.css';
+import './Dashboard.css';
+import { useAuth } from "../contexts/AuthContext"
+import { Link, useHistory } from "react-router-dom"
+import { Card, Button, Alert } from "react-bootstrap"
 
 const App = () => {
   const [picUrl, setPicUrl] = useState([]);
   const [ocrText, setOcrText] = useState([]);
   
   const [isLoading , setIsLoading] = useState(false);
+
+  const [error, setError] = useState("")
+  const { currentUser, logout } = useAuth()
+  const history = useHistory()
+
+  async function handleLogout() {
+    setError("")
+
+    try {
+      await logout()
+      history.push("/login")
+    } catch {
+      setError("Failed to log out")
+    }
+  }
 
   const onDrop = (_,pictureURL)=>{
     setPicUrl(pictureURL);
@@ -28,6 +46,21 @@ const App = () => {
         <h1>My OCR App</h1>
       </header>
 
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Profile</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <strong>Email:</strong> {currentUser.email}
+          <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
+            Update Profile
+          </Link>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        <Button variant="link" onClick={handleLogout}>
+          Log Out
+        </Button>
+      </div>
       <ImageUploader
       withIcon={true}
       withPreview={true}
